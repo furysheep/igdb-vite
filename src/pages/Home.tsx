0e75da2c71ac46b7e007d4162075ff96e8622961
definoob <noahjun511@gmail.com>
2022-04-27 10:15:36 -0300

    infinite scroll

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from 'react'
-import { Center, Container, Grid, GridItem, Spinner } from '@chakra-ui/react'
+import { Container, Grid, Spinner } from '@chakra-ui/react'
 import axios from 'axios'
 import GameItem from 'components/GameItem'
 import { API_DOMAIN } from 'constants'
-import useFetch from 'hooks/useFetch'
 import { Game } from 'types/model'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 const Home = () => {
   const [data, setData] = useState<Game[]>([])
-  useEffect(() => {
+
+  const fetchGames = () => {
     axios
       .post(
         `${API_DOMAIN}/games`,
-        'fields age_ratings.*,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover.*,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_modes,genres.*,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates.*,remakes,remasters,screenshots.*,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos.*,websites; where first_release_date > 1577882711 & rating >= 85; limit 30;',
+        `fields age_ratings.*,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover.*,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_modes,genres.*,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates.*,remakes,remasters,screenshots.*,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos.*,websites; where first_release_date > 1577882711 & rating >= 85; limit 30; offset ${data.length};`,
         {
           headers: {
             'Client-ID': import.meta.env.VITE_CLIENT_ID,
@@ -20,24 +21,26 @@ const Home = () => {
           },
         }
       )
-      .then((res) => {
-        setData(res.data)
-      })
-      .catch((e) => {
-        console.error(e)
-      })
+      .then((res) => setData((prev) => [...prev, ...res.data]))
+      .catch(console.error)
+  }
+
+  useEffect(() => {
+    fetchGames()
   }, [])
 
   return (
     <Container mt={50}>
-      <Grid templateColumns="repeat(3, 1fr)" gap={6}>
-        {data && data.map((game) => <GameItem key={game.id} game={game} />)}
-        <GridItem colSpan={3} h="10">
-          <Center>
-            <Spinner />
-          </Center>
-        </GridItem>
-      </Grid>
+      <InfiniteScroll
+        dataLength={data.length}
+        next={fetchGames}
+        hasMore={true}
+        loader={<Spinner />}
+      >
+        <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+          {data && data.map((game) => <GameItem key={game.id} game={game} />)}
+        </Grid>
+      </InfiniteScroll>
     </Container>
   )
 }
